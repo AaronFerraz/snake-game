502c1c62a372a749e9d3a1ea952a5d3f4cc211f9
AaronFerraz <aaronferrazdpn@gmail.com>
2025-12-14 03:35:27 -0300

    Initial Commit

---
+++ src/main/java/Scenario.java
@@ -0,0 +1,49 @@
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.event.EventHandler;
+
+public class Scenario {
+    private Scene scene;
+    private Group root = new Group();
+    private Snake snake;
+    private Food food;
+
+    public Scenario(Stage primaryStage, Snake snake, Food food){
+        this.scene = new Scene(root, Config.WIDTH, Config.HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Snake Game");
+        primaryStage.show();
+
+        this.snake = snake;
+        this.food = food;
+
+        root.getChildren().add(snake.getHead());
+        root.getChildren().add(food.getFood());
+    }
+
+    public void setKeyPressed(EventHandler<? super KeyEvent> action){
+        this.scene.setOnKeyPressed(action);
+    }
+
+    public void showGameOver(EventLoop eventLoop){
+        TryAgainButton tryAgainButton = new TryAgainButton(e -> {
+            clean();
+            this.add(this.snake.reset());
+            this.add(this.food.getFood());
+            eventLoop.startLoop();
+        });
+
+        this.root.getChildren().add(tryAgainButton);
+    }
+
+    private void clean(){
+        this.root.getChildren().remove(0, this.root.getChildren().size());
+    }
+
+    public void add(Node node){
+        this.root.getChildren().add(node);
+    }
+}
